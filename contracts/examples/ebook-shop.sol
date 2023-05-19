c92f209167f63b2502e7680f304b05a445ee52f8
Roshan <48975233+Pythonberg1997@users.noreply.github.com>
2023-05-19 15:27:33 +0800

    feat: update access control system and code quality refinement (#15)
    * feat: update access control system and code quality refinement
    
    * release: prepare for release v0.0.4

--- contracts/examples/ebook-shop.sol
+++ contracts/examples/ebook-shop.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
 import "../BucketApp.sol";
 import "../ObjectApp.sol";
 import "../GroupApp.sol";
@@ -21,7 +23,7 @@ import "../interface/IERC1155Nontransferable.sol";
  * Only members of the group can get the ebook
  *
  */
-contract EbookShop is BucketApp, ObjectApp, GroupApp {
+contract EbookShop is AccessControl, BucketApp, ObjectApp, GroupApp {
     /*----------------- constants -----------------*/
     // error code
     // 0-3: defined in `baseApp`
@@ -29,16 +31,14 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
     string public constant ERROR_RESOURCE_EXISTED = "5";
     string public constant ERROR_INVALID_PRICE = "6";
     string public constant ERROR_GROUP_NOT_EXISTED = "7";
-    string public constant ERROR_EBOOK_NOT_ONSHELF = "8";
+    string public constant ERROR_EBOOK_NOT_ON_SALE = "8";
     string public constant ERROR_NOT_ENOUGH_VALUE = "9";
-    string public constant ERROR_INVALID_TAX = "10";
+    string public constant ERROR_INVALID_FEE_RATE = "10";
+
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
     /*----------------- storage -----------------*/
-    // admins
-    address public owner;
-    mapping(address => bool) public operators;
-
-    // ERC1155 token for onshelf ebook
+    // ERC1155 token for on-sale ebook
     address public ebookToken;
 
     // system contract
@@ -69,23 +69,11 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
     // ebookId => price
     mapping(uint256 => uint256) public ebookPrice;
 
-    uint256 public tax;
+    uint256 public feeRate;
     mapping(address => uint256) public income;
 
     // PlaceHolder reserve for future use
-    uint256[25] public EbookShopSlots;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "caller is not the owner");
-        _;
-    }
-
-    modifier onlyOperator() {
-        require(msg.sender == owner || _isOperator(msg.sender), "caller is not the owner or operator");
-        _;
-    }
+    uint256[25] _reservedSlots;
 
     function initialize(
         address _crossChain,
@@ -94,23 +82,22 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
         address _groupHub,
         address _paymentAddress,
         uint256 _callbackGasLimit,
-        address _refundAddress,
         uint8 _failureHandleStrategy,
         address _owner,
         address _ebookToken,
-        uint256 _tax
+        uint256 _feeRate
     ) public initializer {
         require(_owner != address(0), string.concat("EbookShop: ", ERROR_INVALID_CALLER));
-        _transferOwnership(_owner);
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
 
-        tax = _tax;
+        feeRate = _feeRate;
         ebookToken = _ebookToken;
         bucketToken = IBucketHub(_bucketHub).ERC721Token();
         objectToken = IObjectHub(_objectHub).ERC721Token();
         groupToken = IGroupHub(_groupHub).ERC721Token();
         memberToken = IGroupHub(_groupHub).ERC1155Token();
 
-        __base_app_init_unchained(_crossChain, _callbackGasLimit, _refundAddress, _failureHandleStrategy);
+        __base_app_init_unchained(_crossChain, _callbackGasLimit, _failureHandleStrategy);
         __bucket_app_init_unchained(_bucketHub, _paymentAddress);
         __group_app_init_unchained(_groupHub);
         __object_app_init_unchained(_objectHub);
@@ -119,18 +106,18 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
     /*----------------- external functions -----------------*/
     function greenfieldCall(
         uint32 status,
-        uint8 resoureceType,
+        uint8 resourceType,
         uint8 operationType,
         uint256 resourceId,
         bytes calldata callbackData
     ) external override(BucketApp, ObjectApp, GroupApp) {
         require(msg.sender == crossChain, string.concat("EbookShop: ", ERROR_INVALID_CALLER));
 
-        if (resoureceType == RESOURCE_BUCKET) {
+        if (resourceType == RESOURCE_BUCKET) {
             _bucketGreenfieldCall(status, operationType, resourceId, callbackData);
-        } else if (resoureceType == RESOURCE_OBJECT) {
+        } else if (resourceType == RESOURCE_OBJECT) {
             _objectGreenfieldCall(status, operationType, resourceId, callbackData);
-        } else if (resoureceType == RESOURCE_GROUP) {
+        } else if (resourceType == RESOURCE_GROUP) {
             _groupGreenfieldCall(status, operationType, resourceId, callbackData);
         } else {
             revert(string.concat("EbookShop: ", ERROR_INVALID_RESOURCE));
@@ -154,7 +141,20 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
         require(seriesId[name] == 0, string.concat("EbookShop: ", ERROR_RESOURCE_EXISTED));
 
         bytes memory _callbackData = bytes(name); // use name as callback data
-        _createBucket(msg.sender, name, visibility, chargedReadQuota, spAddress, expireHeight, sig, _callbackData);
+        _createBucket(
+            msg.sender,
+            name,
+            visibility,
+            paymentAddress,
+            spAddress,
+            expireHeight,
+            sig,
+            chargedReadQuota,
+            msg.sender,
+            failureHandleStrategy,
+            _callbackData,
+            callbackGasLimit
+        );
     }
 
     /**
@@ -170,7 +170,7 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
         require(groupId[name] == 0, string.concat("EbookShop: ", ERROR_RESOURCE_EXISTED));
 
         bytes memory _callbackData = bytes(name); // use name as callback data
-        _createGroup(msg.sender, name, _callbackData);
+        _createGroup(msg.sender, failureHandleStrategy, _callbackData, msg.sender, name, callbackGasLimit);
     }
 
     /**
@@ -198,7 +198,7 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
      * An ERC1155 token will be minted to the buyer.
      */
     function buyEbook(uint256 _ebookId) external payable {
-        require(ebookPrice[_ebookId] > 0, string.concat("EbookShop: ", ERROR_EBOOK_NOT_ONSHELF));
+        require(ebookPrice[_ebookId] > 0, string.concat("EbookShop: ", ERROR_EBOOK_NOT_ON_SALE));
 
         uint256 price = ebookPrice[_ebookId];
         require(msg.value >= price, string.concat("EbookShop: ", ERROR_NOT_ENOUGH_VALUE));
@@ -211,22 +211,22 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
         _member[0] = msg.sender;
         _updateGroup(_owner, _groupId, GroupStorage.UpdateGroupOpType.AddMembers, _member);
 
-        uint256 _income = price * (100 - tax) / 100;
+        uint256 _income = price * (100 - feeRate) / 100;
         income[_owner] += _income;
     }
 
     /**
-     * @dev Provide an ebook's ID to downshelf it.
+     * @dev Provide an ebook's ID to remove it.
      *
-     * The ebook will be removed from the shelf and cannot be bought.
+     * The ebook will be removed from the on-sale list and cannot be bought.
      * Those who have already purchased are not affected.
      */
-    function downshelfEbook(uint256 _ebookId) external {
+    function removeEbook(uint256 _ebookId) external {
         require(
             IERC721NonTransferable(objectToken).ownerOf(_ebookId) == msg.sender,
             string.concat("EbookShop: ", ERROR_INVALID_CALLER)
         );
-        require(ebookPrice[_ebookId] > 0, string.concat("EbookShop: ", ERROR_EBOOK_NOT_ONSHELF));
+        require(ebookPrice[_ebookId] > 0, string.concat("EbookShop: ", ERROR_EBOOK_NOT_ON_SALE));
 
         ebookPrice[_ebookId] = 0;
     }
@@ -296,55 +296,40 @@ contract EbookShop is BucketApp, ObjectApp, GroupApp {
     }
 
     /*----------------- admin functions -----------------*/
-    function transferOwnership(address newOwner) external onlyOwner {
-        require(newOwner != address(0), string.concat("EbookShop: ", ERROR_INVALID_CALLER));
-        _transferOwnership(newOwner);
+    function addOperator(address newOperator) external {
+        grantRole(OPERATOR_ROLE, newOperator);
     }
 
-    function addOperator(address newOperator) public onlyOwner {
-        operators[newOperator] = true;
+    function removeOperator(address operator) external {
+        revokeRole(OPERATOR_ROLE, operator);
     }
 
-    function removeOperator(address operator) public onlyOwner {
-        delete operators[operator];
-    }
-
-    function retryPackage(uint8 resoureceType) external override onlyOperator {
-        if (resoureceType == RESOURCE_BUCKET) {
+    function retryPackage(uint8 resourceType) external override onlyRole(OPERATOR_ROLE) {
+        if (resourceType == RESOURCE_BUCKET) {
             _retryBucketPackage();
-        } else if (resoureceType == RESOURCE_OBJECT) {
+        } else if (resourceType == RESOURCE_OBJECT) {
             _retryObjectPackage();
-        } else if (resoureceType == RESOURCE_GROUP) {
+        } else if (resourceType == RESOURCE_GROUP) {
             _retryGroupPackage();
         } else {
             revert(string.concat("EbookShop: ", ERROR_INVALID_RESOURCE));
         }
     }
 
-    function skipPackage(uint8 resoureceType) external override onlyOperator {
-        if (resoureceType == RESOURCE_BUCKET) {
+    function skipPackage(uint8 resourceType) external override onlyRole(OPERATOR_ROLE) {
+        if (resourceType == RESOURCE_BUCKET) {
             _skipBucketPackage();
-        } else if (resoureceType == RESOURCE_OBJECT) {
+        } else if (resourceType == RESOURCE_OBJECT) {
             _skipObjectPackage();
-        } else if (resoureceType == RESOURCE_GROUP) {
+        } else if (resourceType == RESOURCE_GROUP) {
             _skipGroupPackage();
         } else {
             revert(string.concat("EbookShop: ", ERROR_INVALID_RESOURCE));
         }
     }
 
-    function setTax(uint256 _tax) external onlyOwner {
-        require(_tax < 100, string.concat("EbookShop: ", ERROR_INVALID_TAX));
-        tax = _tax;
-    }
-
-    /*----------------- internal functions -----------------*/
-    function _transferOwnership(address newOwner) internal {
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-    function _isOperator(address account) internal view returns (bool) {
-        return operators[account];
+    function setFeeRate(uint256 _feeRate) external onlyRole(OPERATOR_ROLE) {
+        require(_feeRate < 100, string.concat("EbookShop: ", ERROR_INVALID_FEE_RATE));
+        feeRate = _feeRate;
     }
 }
